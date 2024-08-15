ace652d8a7e7e9400ec850c6942a34b8022f7a99
Ser-An-Pol <seranpol@icloud.com>
2024-08-15 10:46:01 +0300

    7kyu/ => Complete 7 kyu + 1

--- 7kyu/EnoughIsEnough_c.java
+++ 7kyu/EnoughIsEnough_c.java
@@ -9,28 +9,37 @@ For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], y
 With list [20,37,20,21] and number 1, the result would be [20,37,21].
  */
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class EnoughIsEnough_c {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(deleteNth( new int[] { 1, 3, 7 }, 0 )));
     }
 
-    public static int[] deleteNth(int[] elements, int maxOccurrences) {
-		ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < elements.length; i++) list.add(elements[i]);
+    // public static int[] deleteNth(int[] elements, int maxOccurrences) {
+	// 	ArrayList<Integer> list = new ArrayList<>();
+    //     for (int i = 0; i < elements.length; i++) list.add(elements[i]);
 
-        for (int i = 0; i < list.size(); i++) {
-            Integer el = list.get(i);
-            int count = (int)list.stream().filter(in -> in == el).count();
-            while (count > maxOccurrences) {
-                list.remove(list.lastIndexOf(el));
-                count--;
-            }
-        }
-        int[] res = new int[list.size()];
-        for (int i = 0; i < res.length; i++) res[i] = list.get(i);
-		return res;
+    //     for (int i = 0; i < list.size(); i++) {
+    //         Integer el = list.get(i);
+    //         int count = (int)list.stream().filter(in -> in == el).count();
+    //         while (count > maxOccurrences) {
+    //             list.remove(list.lastIndexOf(el));
+    //             count--;
+    //         }
+    //     }
+    //     int[] res = new int[list.size()];
+    //     for (int i = 0; i < res.length; i++) res[i] = list.get(i);
+	// 	return res;
+	// }
+
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+		Map<Integer, Integer> occurrence = new HashMap<>();
+		return IntStream.of(elements)
+			.filter(motif -> occurrence.merge(motif, 1, Integer::sum) <= maxOccurrences)
+			.toArray();
 	}
 }
